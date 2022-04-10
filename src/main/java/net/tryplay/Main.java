ab73bbdf288b7828c668ba1beb23460dd59f1619
alfonsoLeandro <leandroalfonsoporley@gmail.com>
2022-04-09 21:39:19 -0300

    Añadido listener que previene entity spawn. Removida clase no utilizada. Arreglado plugin.yml. Añadido método send en clase Main. cambiado placeholderapi = null por placeholderapi.isenabled. Cambiado player.getPlayer por player.

--- src/main/java/net/tryplay/Main.java
+++ src/main/java/net/tryplay/Main.java
@@ -1,45 +1,53 @@
 package net.tryplay;
 
+import net.tryplay.listeners.Listeners;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.TimeUnit;
 
 public final class Main extends JavaPlugin implements Listener {
+
     private Saver saver;
+
     @Override
     public void onEnable() {
-
-        Bukkit.getPluginManager().registerEvents(this,this);
+        this.saver = new Saver(this);
+        Bukkit.getPluginManager().registerEvents(new Listeners(this),this);
         if(!getDataFolder().exists()){
             getDataFolder().mkdir();
         }
-        if(Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+        if(Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
             new placeholders(this).register();
         }
-        this.saver = new Saver(this);
-        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Plugin Activado");
+        send(Bukkit.getConsoleSender(), "&aPlugin activado");
 
     }
 
     @Override
     public void onDisable() {
         this.saver.save();
-        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Plugin Desactivado");
+        send(Bukkit.getConsoleSender(), "&cPlugin desactivado");
+    }
+
+    public void send(CommandSender sender, String message){
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
     }
 
 
-    public String getPlace(Player e){
-        return TimeUnit.MILLISECONDS.toSeconds(( saver.getPlace(e.getPlayer()) + 60000) - System.currentTimeMillis())+ " segundos";
+    public String getPlace(Player player){
+        return TimeUnit.MILLISECONDS.toSeconds(( this.saver.getPlace(player) + 60000) - System.currentTimeMillis())+ " segundos";
     }
-    public String getBreak(Player e){
-        return TimeUnit.MILLISECONDS.toSeconds(( saver.getBreak(e.getPlayer()) + 60000) - System.currentTimeMillis())+ " segundos";
+    public String getBreak(Player player){
+        return TimeUnit.MILLISECONDS.toSeconds(( this.saver.getBreak(player) + 60000) - System.currentTimeMillis())+ " segundos";
     }
 
     public Saver getSaver(){
--- src/main/java/net/tryplay/listeners/Listeners.java
+++ src/main/java/net/tryplay/listeners/Listeners.java
@@ -2,10 +2,12 @@ package net.tryplay.listeners;
 
 import net.tryplay.Main;
 import net.tryplay.Saver;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
 
 public class Listeners implements Listener {
 
@@ -23,18 +25,26 @@ public class Listeners implements Listener {
             this.saver.saveBreak(e.getPlayer(), System.currentTimeMillis());
         }else{
             e.setCancelled(true);
-            e.getPlayer().sendMessage("§4§lPuedes volver a romper un bloque en "+ this.plugin.getBreak(e.getPlayer()));
+            this.plugin.send(e.getPlayer(),
+                    "&4&lPuedes volver a romper un bloque en "+this.plugin.getBreak(e.getPlayer()));
         }
 
     }
+
     @EventHandler
     public void blockPlace(BlockPlaceEvent e){
         if((System.currentTimeMillis() >= this.saver.getPlace(e.getPlayer()) + 60000)){
             this.saver.savePlace(e.getPlayer(), System.currentTimeMillis());
         }else{
             e.setCancelled(true);
-            e.getPlayer().sendMessage("§4§lPuedes volver a colocar un bloque en "+ this.plugin.getPlace(e.getPlayer()));
+            this.plugin.send(e.getPlayer(),
+                    "&4&lPuedes volver a colocar un bloque en "+ this.plugin.getPlace(e.getPlayer()));
         }
+    }
+
+    @EventHandler
+    public void entitySpawn(EntitySpawnEvent event){
+        if(!event.getEntityType().equals(EntityType.PLAYER)) event.setCancelled(true);
     }
 
 }
